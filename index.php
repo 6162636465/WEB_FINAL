d1aef10ef2d024caeaacde87ba291901468d755e
javier.alvarez Reyes <40539959+6162636465@users.noreply.github.com>
2023-06-25 14:49:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div id="left-sidebar">
+        <button class="image-button">
+            <img src="LogosyPaginas/image1.png" alt="Imagen 1">
+        </button>
+        <button class="image-button">
+            <img src="LogosyPaginas/image2.png" alt="Imagen 2">
+        </button>
+        <!-- Coloca más botones según sea necesario -->
+    </div>
+
+    <div id="center-content">
+        <h1>ALÓ PROFETAS?</h1>
+        <div id="center-images">
+            <img src="Imagenes/center_image1.png" alt="Imagen central 1">
+            <img src="Imagenes/center_image2.png" alt="Imagen central 2">
+        </div>
+        <div id="center-buttons">
+            <button class="center-button">Reseña de clases</button>
+            <button class="center-button">Lista de profesores</button>
+        </div>
+    </div>
+
+    <div id="right-sidebar">
+        <button class="image-button">
+            <img src="LogosyPaginas/image3.png" alt="Imagen 3">
+        </button>
+        <button class="image-button">
+            <img src="LogosyPaginas/image4.png" alt="Imagen 4">
+        </button>
+        <!-- Coloca más botones según sea necesario -->
+    </div>
+</body>
+</html>
